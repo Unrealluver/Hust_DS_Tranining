c3d0afaec1cefdaea54eefd59bb0e16a3de7c17e
LSune <1653673717@qq.com>
2018-09-17 17:49:22 +0800

    add 2.11

--- Chapter2/solutions.c
+++ Chapter2/solutions.c
@@ -15,6 +15,26 @@ Status DeleteK(SqList &a, int i, int k) {
   }
 }
 
+// 2.11 Insert the element x to the ordered(from small to big) list.
+Status InsertToOrderedList(SqList &a, int x) {
+    if (a.length + 1 > a.listsize) {
+        ElemType *newBase = (ElemType *)realloc(a.elem,
+                                      (a.listsize + LISTINCREMENT) * sizeof (ElemType));
+        if (!newBase) return OVERFLOW;
+        a.elem = newBase;
+        a.listsize += LISTINCREMENT;
+    }
+    for (int i = 0; i < a.length - 1; i++) {
+        if ((a.elem[i] < x && a.elem[i+1] > x) || (i == 0 && a.elem[i] > x) || (i == a.length - 1 && a.elem[i] < x)) {
+            for (int j = a.length; j > i + 1; j--)
+                a.elem[j] = a.elem[j-1];
+            a.elem[i+1] = x;
+            a.length++;
+            return OK;
+        }
+    }
+}
+
 int main () {
   printf("hello");
 }
