82f0f1b4fb5dae3bef748d3e0c673afc0f45d3c8
Unrealluver <1653673717@qq.com>
2019-01-08 15:14:29 +0800

    Cpp extend test final

--- CppFolder/extendTest.cpp
+++ CppFolder/extendTest.cpp
@@ -1,52 +1,124 @@
 #include <stdio.h>
 #include <iostream>
-#include "./extendTest.h"
+#include "extendTest.h"
+
 using namespace std;
 
-Point :: Point(int x, int y) {
-    this -> x = x;
-    this -> y = y;
+Point::Point() {
+    x = 0;
+    y = 0;
+}
+Point::Point(int x, int y) {
+    this->x = x;
+    this->y = y;
 }
 
-Point :: Point(int x, int y, int varA) {
+Point::Point(int x, int y, int varA) {
     Point(x, y);
-    this -> varA = varA;
+    this->varA = varA;
 }
 
-void Point :: draw() {
+void Point::virMethod() {
+    cout << "this is the virtual method of Point"
+         << endl;
+
+}
+
+void DoublePoint::virMethod() {
+    cout << "this is the virtual method of DPoint"
+         << endl;
+}
+
+void TriplePoint::virMethod() {
+    cout << "this is the virtual method of TPoint"
+         << endl;
+}
+
+void SonPoint::virMethod() {
+    cout << "this is the virtual method of SPoint"
+         << endl;
+}
+
+void Point::printWhich() {
+    virMethod();
+}
+
+void Point::retake() {
+    cout << "this is the no Param method"
+         << endl;
+}
+
+void Point::retake(bool flag) {
+    if (flag)
+        cout << "this is the method with params"
+             << endl;
+}
+
+void DoublePoint::retake() {
+    cout << "this is the method of DoublePoint"
+         << endl;
+}
+
+void DoublePoint::virRetake() {
+    Point::virRetake();
+}
+
+void DoublePoint::sameMethod() {
+    cout << "this is the same method that provided by D"
+         << endl;
+}
+
+void TriplePoint::sameMethod() {
+    cout << "this is the same method that provided by T"
+         << endl;
+}
+
+void Point::virRetake() {
+    cout << "this is the virtual method with no params"
+         << endl;
+}
+
+void Point::virRetake(bool flag) {
+    if (flag)
+        cout << "this is the virtual method with params"
+             << endl;
+}
+void Point::draw() {
     cout << "this is the output of Point: x:"
-        << x << "\t y:" << y << endl;
+         << x << "\t y:" << y << endl;
 }
 
-Line :: Line(int x, int y, int length, int varB) : Point(x, y){
-    this -> length = length;
-    this -> varB = varB;
+Line::Line(int x, int y, int length, int varB) : Point(x, y) {
+    this->length = length;
+    this->varB = varB;
 }
 
-void Line :: draw() {
+void Line::draw() {
     cout << "this is the output of Line: x:"
-        << x << "\t y:" << y << "\t length:" << length << endl;
+         << x << "\t y:" << y << "\t length:" << length << endl;
 }
 
-Circle :: Circle(int x, int y, int radius, int varC) : Point(x, y) {
-    this -> radius = radius;
-    this -> varC = varC;
+Circle::Circle(int x, int y, int radius, int varC) : Point(x, y) {
+    this->radius = radius;
+    this->varC = varC;
 }
 
-void Circle :: draw() {
+void Circle::draw() {
     cout << "this is the output of Circle: x:"
-        << x << "\t y:" << y << "\t radius:" << radius << endl;
+         << x << "\t y:" << y << "\t radius:" << radius << endl;
 }
 
-Multi :: Multi(int x, int y, int radius, int length, int center, int varD) : Circle(x + 1, y + 1, radius + 1, 0), Line(x + 2, y + 2, length + 2, 0), Point(x, y) {
-    this -> center = center;
-    this -> varD = varD;
+Multi::Multi(int x, int y, int radius, int length, int center, int varD) : Circle(x + 1, y + 1, radius + 1, 0),
+                                                                           Point(x, y),
+                                                                           Line(x + 2, y + 2, length + 2, 0) {
+    this->center = center;
+    this->varD = varD;
 }
 
-void Multi :: draw() {
+void Multi::draw() {
     cout << "this is the output of Multi: x:"
-        << x << "\t y:" << y << "\t radius:" <<
-        radius << "\t length:" << length << "\t center:" << center << endl;
+         << x << "\t y:" << y << "\t radius:" <<
+         radius << "\t length:" << length << "\t center:" << center << endl;
 }
 /** Question 1:
  *
@@ -68,11 +140,50 @@ void Multi :: draw() {
  *  有怎样的显示呢?
  */
 
+/** Question 3:
+ *
+ *      采用虚函数的菱形继承中,两个直接
+ *  父类分别对于目标虚函数进行 Override
+ *  子类不处理该函数, 会发生什么, 子类
+ *  同样 Override 会发生什么.
+ */
+
+/** Question 4:
+ *      两个父类中含有同名方法, 子类同时
+ *  继承这两个类, 调用该方法会发生什么,
+ *  若该同名方法中有一个被声明为 virtual
+ *  会发生什么, 是不是子类必须也 Override
+ *  这个方法呢?
+ */
+
+/** Question 5:
+ *      在父类中声明一个a方法,再同时声明
+ *  一个重载的a方法,子类中声明一个同名的
+ *  a方法,此时调用适用于父类中参数类型的
+ *  a方法,会如何,如果为父类的两个a方法添
+ *  加virtual属性, 那么它们还能算作是重载
+ *  吗?
+ */
+
+/** Question 6:
+ *
+ *      虚基类继承下被声明为虚基类的父类对象
+ *  与子类对象本身在地址上是否具有不连续性,
+ *  以及4个字节大小的虚基类指针如何体现.
+ */
+
+/** Question 7:
+ *
+ *      虚基类的构造函数和一般类的构造函数同时
+ *  出现在初始化列表当中时,他们的初始化顺序如何
+ *  ?
+ */
+
 /**
  * Plus 1:
  * CPP 的虚继承机制:
  *
- *      父类中A函数调用B函数, 在子 类中覆写
+ *      父类中A函数调用B函数, 在子类中覆写
  * B函数, 此时若不再B函数前声明virtual
  * , 则会调用父类中的B函数而非子类覆写
  * 过的.
@@ -109,6 +220,100 @@ int main() {
      * 父类.至此,虚继承完美解决了多重继承情况
      * 下的下级子类持有多个父类变量实体的情况.
      */
+
+    // local test
+    DoublePoint doublePoint;
+    doublePoint.virMethod();
+    TriplePoint triplePoint;
+    triplePoint.virMethod();
+    // pointer test
+    Point * pointP;
+    pointP = &doublePoint;
+    printWhich(pointP);
+    // 注: 此处如果是私有继承, 子类对象无法赋给父类指针;
+
+    // reference test
+    Point &pointR = triplePoint;
+    printWhich(&pointR);
+
+    // final son test
+    SonPoint sonPoint;
+    sonPoint.virMethod();
+    // (子类无处理)error: request for member ‘virMethod’ is ambiguous
+    // (子类仅声明)error: undefined reference to `vtable for SonPoint'
+    // (子类声明且定义) this is the virtual method of SPoint
+    /** Conclusion 3:
+     *
+     *      子类此处如果不对从父类继承下来的virtual方法进行处理,
+     *  在调用的时候会报歧义错误.此处逻辑很好理解, 因为子类当中找
+     *  不到相关实现, 所以会在父类中寻找, 而此时恰好两个父类都对其
+     *  有所定义, 所以会报二义性错误.
+     *
+     *      子类仅仅只是声明而不定义的话则会有所不同, 因为子类当中
+     *  有对此虚函数的声明, 所以编译器会去虚函数表中寻找该虚函数,
+     *  但由于我们没有对其进行定义, 于是会报虚函数表错误, 告诉我们
+     *  此虚函数在表中缺少相应的定义.
+     *
+     *      子类声明且提供相关的定义, 此时可以正常运行.
+     */
+
+    // 成员方法测试
+    doublePoint.printWhich();
+    triplePoint.printWhich();
+    sonPoint.DoublePoint::printWhich();
+    sonPoint.TriplePoint::printWhich();
+    // 注: 此处如果不注明作用域会因不知调用哪一个直接父类的方法而报错
+//    sonPoint.Point::printWhich();
+    // 注: 此处不可以直接调用源头基类的方法.会报二义性错误
+    // error: ‘Point’ is an ambiguous base of ‘SonPoint’
+
+    // 测试来自不同父类的同样方法
+//    sonPoint.sameMethod();
+    //  error: request for member ‘sameMethod’ is ambiguous
+    // 不出我们所料报了二义性的错误.
+
+    // 当其中一个父类的方法被声明为virtual时
+//    sonPoint.sameMethod();
+    //  error: request for member ‘sameMethod’ is ambiguous
+    //  任然是报了二义性的错误.virtual并未使该方法再优先级上有改变
+
+    /** Conclusion 4:
+     *
+     *      子类继承的父类中有同名方法时, 必须表明该方法来自于哪一个父类,
+     *  不然就会报二义性错误.
+     *
+     *      就算是表明为virtual也不能改变这一点.
+     *      virtual只是表明该方法的存储位置有改变, 单独提供虚函数vptr指向他,
+     *  但并不代表该方法再优先级上有何变化, 再相同的作用域当中一样可以发生重载,
+     *  一样会有二义性的情况产生.
+     *
+     */
+
+    // 测试重载覆盖
+    doublePoint.retake();
+    doublePoint.Point::retake(true);
+//    doublePoint.retake(true);
+    // error: no matching function for call to ‘DoublePoint::retake(bool)’
+    // 会提示找不到匹配的函数
+
+    doublePoint.virRetake();
+//    doublePoint.virRetake(true);
+    // error: no matching function for call to ‘DoublePoint::virRetake(bool)’
+    // 会提示找不到匹配的项
+
+    // 测试两个父类中
+    /** Conclusion 5:
+     *
+     *      如果我们再父类当中声明了重载的两个方法, 然后再由子类覆盖
+     *  其中的某一个方法, 那么当我们在调用另一个方法的时候, 编译器会
+     *  报错告诉我们再子类的类体内无法找到匹配的方法.我们只能再前面
+     *  声明一下该方法的作用域
+     *
+     *      就算我们声明为virtual, 也不会改变函数重载的关系,同时,
+     *  也不会影响子类当中因此而生的无匹配函数的问题,该问题同样会在
+     *  这里出现.
+     */
+
 
     cout << "hello\n";
 }
--- CppFolder/extendTest.h
+++ CppFolder/extendTest.h
@@ -1,40 +1,120 @@
+// 该类为我们实验探究时所需的源头基类
 class Point {
-    private:
-        int varA;
-    public:
-        int x, y;
-        
-        Point(int x, int y); 
-        Point(int x, int y, int varA);
-        void draw();
+private:
+    int varA;
+public:
+    // 基类内重要的基础变量
+    int x, y;
+
+    // 提供一个无参构造方法
+    Point();
+
+    // 为我们的基类提供两种构造方法
+    Point(int x, int y);
+
+    Point(int x, int y, int varA);
+
+    // 用来测试的virtual方法
+    virtual void virMethod();
+
+    // 用来测试重载的方法
+    void retake();
+
+    void retake(bool flag);
+
+    // 用来测试virtual重载的方法
+    virtual void virRetake();
+
+    virtual void virRetake(bool flag);
+
+    virtual // 用来供后续类 Override 的draw方法
+    void draw();
+
+    void printWhich();
 };
 
+// 菱形继承中的一个中间类
 class Line : virtual public Point {
-    private:
-        int varB;
-    public:
-        int length;
-        
-        Line(int x, int y, int length, int varB);
-        void draw();
+private:
+    int varB;
+public:
+    // 中间类的特征变量
+    int length;
+
+    // 中间类的构造函数
+    Line(int x, int y, int length, int varB);
+
+    // 中间类对于draw方法的重载
+    void draw();
 };
 
+// 菱形继承当中的另外一个中间类
 class Circle : virtual public Point {
-    private:
-        int varC;
-    public:
-        int radius;
+private:
+    int varC;
+public:
+    // 中间类的特征变量
+    int radius;
 
-        Circle(int x, int y, int radius, int varC);
-        void draw();
+    // 中间类的构造函数
+    Circle(int x, int y, int radius, int varC);
+
+    // 中间类对于draw方法的重载
+    void draw();
 };
 
+// 菱形继承中的最下游子类, 同时继承自Circle, Line两个父类
 class Multi : Circle, Line {
-    private:
-        int varD;
-    public:
-        int center;
+private:
+    int varD;
+public:
+    // 下游子类的特征变量
+    int center;
 
-        Multi(int x, int y, int radius, int length, int center, int varD);
-        void draw();
+    // 下游子类的内部方法
+    Multi(int x, int y, int radius, int length, int center, int varD);
+
+    // 下游子类对于 draw 方法的 Override
+    void draw();
 };
+
+// 用于测试虚函数的类
+class DoublePoint : public Point {
+private:
+    int varE;
+public:
+    virtual void virMethod();
+
+    // 重载re-take方法
+    void retake();
+
+    // 重载virtual的retake 方法
+    void virRetake() override;
+
+    // 和另一个类提供相同的方法
+    virtual void sameMethod();
+
+};
+
+// 用于测试虚函数的类2
+class TriplePoint : public Point {
+private:
+    int varF;
+public:
+    virtual void virMethod();
+
+    // 和另一个类提供同名的方法
+    void sameMethod();
+};
+
+// 用于测试虚函数的最子类
+class SonPoint : public DoublePoint, public TriplePoint {
+private:
+    int varG;
+public:
+    virtual void virMethod();
+};
+
+void printWhich(Point * point) {
+    point->virMethod();
+}
